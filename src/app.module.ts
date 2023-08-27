c49663beae1e90533a2315617c648bf197589b87
arpan <arpansadhu03@gmail.com>
2023-08-27 14:59:37 +0530

    db init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './Database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal:true}),
-    AuthModule, HealthModule],
+    AuthModule, HealthModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
